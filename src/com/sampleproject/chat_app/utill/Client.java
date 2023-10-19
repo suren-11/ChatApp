33c8d93e7f504177845353ae3e50f2db734dc8bd
suren-11 <presanth11@gmail.com>
2023-10-19 16:14:41 +0530

    creat client functions

--- src/com/sampleproject/chat_app/utill/Client.java
+++ src/com/sampleproject/chat_app/utill/Client.java
@@ -4,8 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
@@ -17,4 +16,26 @@ public class Client {
     private BufferedImage bufferedImage;
     private Image fxImage;
     private VBox sendersVBox;
+
+    public Client(Socket socket,String userName, VBox vBox){
+        try {
+            this.socket = socket;
+            this.bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.userName = userName;
+            this.vBox = vBox;
+        } catch (IOException e) {
+            closeAll(this.socket,this.bufferedReader,this.bufferedWriter);
+        }
+    }
+    public void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if (bufferedReader!=null) bufferedReader.close();
+            if (bufferedWriter!=null) bufferedWriter.close();
+            if (socket!=null) socket.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
