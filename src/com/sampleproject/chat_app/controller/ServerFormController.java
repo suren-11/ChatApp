751cc5280ad2f429e696c7e5a2a3904c30cecb23
suren-11 <presanth11@gmail.com>
2023-10-20 19:29:45 +0530

    updated display right methods in server form controller

--- src/com/sampleproject/chat_app/controller/ServerFormController.java
+++ src/com/sampleproject/chat_app/controller/ServerFormController.java
@@ -29,9 +29,9 @@ public class ServerFormController {
             try{
                 ServerSocket serverSocket = new ServerSocket(5000);
                 System.out.println("server started..");
+                displayMessageOnRight("Server Started..");
                 server = new Server(serverSocket);
                 server.startServer(vBoxMsgS);
-
             }catch (IOException e){
                 e.printStackTrace();
             }
@@ -44,7 +44,7 @@ public class ServerFormController {
             }
         });
     }
-    public static void displayMessageOnRight(String messageToSend, VBox vBox){
+    public static void displayMessageOnRight(String messageToSend){
         if (!messageToSend.isEmpty()){
             HBox hBox = new HBox();
             hBox.setAlignment(Pos.CENTER_RIGHT);
@@ -55,9 +55,6 @@ public class ServerFormController {
             textFlow.setPadding(new Insets(5,5,5,10));
             msgText.setFill(Color.WHITE);
             hBox.getChildren().add(textFlow);
-            Platform.runLater(()->{
-                vBox.getChildren().add(hBox);
-            });
         }
     }
 
